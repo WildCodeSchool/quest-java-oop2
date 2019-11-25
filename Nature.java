c6f34c1de30a7f9426e4a496dca51bb1e691dc40
Benoît Hubert <benoithubert@gmail.com>
2019-11-25 08:12:59 +0100

    Fix typos

--- Nature.java
+++ Nature.java
@@ -4,17 +4,17 @@ public class Nature {
 
     public static void main(String[] args) {
 
-        // donald receives a new instance of classe Duck
+        // donald receives a reference to a new instance of class Duck
         Duck donald = new Duck("Donald"); 
         // setter usage
         donald.setAge(84);
         // getters usage
-        System.out.println("1. Duck name is : " + donald.getName());
+        System.out.println("1. Duck name is: " + donald.getName());
         System.out.println("2. He is " + String.valueOf(donald.getAge()));
         System.out.println("3. He sings " + donald.sing());
 
         Eagle thorondor = new Eagle("Thorondor");
-        System.out.println("4. Eagle name is : " + thorondor.getName());
+        System.out.println("4. Eagle name is: " + thorondor.getName());
         System.out.println("5. He is " + String.valueOf(thorondor.getAge()));
         System.out.println("6. He sings " + thorondor.sing());
         
@@ -24,7 +24,7 @@ public class Nature {
         birds.add(donald);
         birds.add(thorondor);
         
-        System.out.println("7. Oiseaux de la liste : ");
+        System.out.println("7. Birds in the list: ");
         // iterate over the list
         for (Bird bird : birds) {
             System.out.println("- " + bird.getName());
