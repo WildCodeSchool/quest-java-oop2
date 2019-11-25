c96106b7c80a8ffa6611b3a2b0addccf75ecf941
Benoît Hubert <benoithubert@gmail.com>
2019-11-25 08:11:35 +0100

    Translate comments

--- Nature.java
+++ Nature.java
@@ -4,11 +4,11 @@ public class Nature {
 
     public static void main(String[] args) {
 
-        // la référence riri reçoit une nouvelle instance de la classe Duck
+        // donald receives a new instance of classe Duck
         Duck donald = new Duck("Donald"); 
-        // utilisation d'un setter    
+        // setter usage
         donald.setAge(84);
-        // utilisation de getters      
+        // getters usage
         System.out.println("1. Duck name is : " + donald.getName());
         System.out.println("2. He is " + String.valueOf(donald.getAge()));
         System.out.println("3. He sings " + donald.sing());
@@ -18,14 +18,14 @@ public class Nature {
         System.out.println("5. He is " + String.valueOf(thorondor.getAge()));
         System.out.println("6. He sings " + thorondor.sing());
         
-        // comme riri et thorondor sont des enfants de Bird
-        // il est possible de les stocker dans une même ArrayList
+        // since Donald and Thorondor are both instances of Bird, they can be
+        // stored in the same ArrayList (meant to contain Bird instances)
         ArrayList<Bird> birds = new ArrayList<>();
         birds.add(donald);
         birds.add(thorondor);
         
         System.out.println("7. Oiseaux de la liste : ");
-        // parcours de la liste
+        // iterate over the list
         for (Bird bird : birds) {
             System.out.println("- " + bird.getName());
         }
